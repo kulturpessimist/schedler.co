6ffa772c1c3e66d17227794e258bdb16f144581e
Alexander Schedler <alex@schedler.co>
2022-03-08 15:49:39 +0100

    cleanuop

--- src/js/index.js
+++ src/js/index.js
@@ -1,94 +1,70 @@
-import storybook from "./storybook"
-// frameworks and libs
+import 
+
+// import { createApp } from "petite-vue"
+import anime from "animejs/lib/anime.es.js"
 import { annotate, annotationGroup } from "rough-notation"
-import "alpinejs"
-import "impress.js"
-import version from "../version.json"
-// styles...
-// ...sass
-import "../css/bulma.scss"
-// ...css
-import "@fortawesome/fontawesome-free/css/brands.css"
-import "@fortawesome/fontawesome-free/css/solid.css"
-import "@fortawesome/fontawesome-free/css/fontawesome.css"
-import "animate.css"
-import "../fonts/fonts.css"
-import "../css/style.css"
-import "../css/jobs.css"
-//
-const app = {
-  data() {
-    return {
-      showImprint: false,
-      showImprint1: false,
-      showImprint2: false,
-      showImprint3: false,
-      api: undefined,
-      storybook,
-      window: {
-        width: 0,
-        height: 0,
-      },
-      version,
-      //
-      init() {
-        console.log("mounted")
-        this.api = impress()
-        document.body.addEventListener("impress:init", this.stepInit)
-        document.body.addEventListener("impress:stepenter", this.stepEnter)
-        document.body.addEventListener("impress:stepleave", this.stepLeave)
-        document.body.addEventListener("impress:steprefresh", this.stepRefresh)
-        this.window.width = window.innerWidth
-        this.window.height = window.innerHeight
-        // let alpine run and after that init impress
-        setTimeout(this.api.init, 1)
-      },
-
-      stepInit(event) {
-        console.log("impress:stepinit", event)
-      },
-      stepLeave(event) {
-        /* console.log("impress:stepleave", event) */
-        const chapter = event.target.id
-        if (chapter in storybook && "destroy" in storybook[chapter]) {
-          storybook[chapter].destroy()
-        }
-      },
-      stepEnter(event) {
-        /* console.log("impress:stepenter", event) */
-        const chapter = event.target.id
-        if (chapter in storybook && "init" in storybook[chapter]) {
-          storybook[chapter].init()
-        }
-      },
-      stepRefresh(event) {
-        /* console.log("impress:steprefresh", event) */
-      },
-
-      previous() {
-        this.api.prev()
-      },
-      next() {
-        this.api.next()
-      },
-
-      open() {
-        this.showImprint = true
-        const e = document.querySelector(" mark")
-        const annotation = annotate(e, {
-          type: "highlight",
-          color: "#00990099",
-        })
-        annotation.show()
-      },
-      close() {
-        this.showImprint = false
-      },
-      isOpen() {
-        return this.showImprint === true
-      },
-    }
-  },
+import pages from "./pages"
+// createApp({}).mount("body")
+const annotateAllTheThings = () => {
+  const e = document.querySelectorAll("mark")
+  const annotations = []
+  for (let m of e) {
+    console.log(m)
+    annotations.push(annotate(m, { type: "highlight", color: "#000066" }))
+  }
+  const ag = annotationGroup(annotations)
+  ag.show()
 }
 
-window.app = app
+window.anime = anime
+//
+document.querySelector("pre").innerHTML = pages[0]
+//
+
+setTimeout(annotateAllTheThings, 500)
+
+/* setTimeout(() => {
+  document.querySelector("pre").innerHTML = pages[1]
+  setTimeout(annotateAllTheThings, 500)
+}, 5000)
+ */
+
+document.querySelector("#back").addEventListener("click", () => {})
+document.querySelector("#forward").addEventListener("click", (event) => {
+  anime({
+    targets: "main",
+    keyframes: [{ translateY: -1000 }, { translateX: -800 }],
+    duration: 4000,
+    easing: "easeOutElastic(1, .8)",
+  })
+
+  event.stopImmediatePropagation()
+  event.preventDefault()
+  return false
+})
+
+document.querySelector("#options").addEventListener("click", () => {
+  console.log("clicked")
+  let tx = document.querySelector("pre").innerText
+  let t = tx.split("")
+  let p = []
+  t.forEach((v, i, a) => {
+    if (v.match(/\S/)) {
+      p.push(i)
+    }
+  })
+  for (let c = 0; c < 10; c++) {
+    const item = p[Math.floor(Math.random() * p.length)]
+    t[item] = Math.random().toString(36).substr(2, 1)
+    window.requestAnimationFrame(() => {
+      document.querySelector("pre").innerText = t.join("")
+    })
+  } /*
+  const regex = RegExp(/\S/, "g")
+  const x = regex.test(t)
+  console.log(x)
+  t = t.replace(/\S/, "*")
+  console.log(t)
+  document.querySelector("pre").innerHTML = t
+  */
+})
